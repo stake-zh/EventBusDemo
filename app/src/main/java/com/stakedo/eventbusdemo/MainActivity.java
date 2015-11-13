75130cc4ee803411d5c707cbc926d6c9b58116ed
starkey_zh <starkey86@163.com>
2015-11-13 16:29:23 +0800

    init

---
+++ app/src/main/java/com/stakedo/eventbusdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.stakedo.eventbusdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import de.greenrobot.event.EventBus;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+
+    public void onEventMainThread(Event event) {
+        Log.d(TAG, "onEventMainThread: "+ event.msg);
+    }
+
+
+    static class Event {
+        String msg;
+    }
+}
